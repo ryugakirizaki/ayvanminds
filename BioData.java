445daf8ca29386cf25b707f8f198b392f21227fc
ryugakirizaki <88543427+ryugakirizaki@users.noreply.github.com>
2021-08-06 18:19:50 +0530

    Create BioData.java

---
+++ BioData.java
@@ -0,0 +1,9 @@
+//Program to print my biodata
+class BioData {
+    public static void main(String args[]) {
+        System.out.println("Name: Hemvedh");
+        System.out.println("Date of birth: 04/06/2003");
+        System.out.println("Blood Group: B+");
+        System.out.println("State: Telangana");
+    }
+}
